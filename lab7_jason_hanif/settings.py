724b5aef1f4ef29d1ff3ac80935aa7002708e09d
jasonirvine76 <jason.irvine@ui.ac.id>
2024-11-03 16:56:52 +0700

    fix: allowed all hosts

--- lab7_jason_hanif/settings.py
+++ lab7_jason_hanif/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-709%oi2szh8le@fp*qr_jt6c(ixh&iou@fpe-m7v)azw&7z$#&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
